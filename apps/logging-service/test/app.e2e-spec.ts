c0b6264fb83ca3c4bb84de7e9c1d99d1ad51896e
Wisdom <urangyorkwisdom@gmail.com>
2025-09-15 17:21:24 +0100

    created monorepo microservice

--- apps/logging-service/test/app.e2e-spec.ts
+++ apps/logging-service/test/app.e2e-spec.ts
@@ -1,15 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { LoggingServiceModule } from './../src/logging-service.module';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+describe('LoggingServiceController (e2e)', () => {
+  let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [LoggingServiceModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
