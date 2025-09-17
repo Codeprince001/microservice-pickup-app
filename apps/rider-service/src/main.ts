17bf2811c94db9dfd9fa2a2647e94450eeaffd3f
Wisdom <urangyorkwisdom@gmail.com>
2025-09-17 05:08:10 +0100

    chore(rider-service): bootstrap RiderService as a TCP microservice on port 4002

--- apps/rider-service/src/main.ts
+++ apps/rider-service/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { RiderServiceModule } from './rider-service.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(RiderServiceModule);
-  await app.listen(process.env.port ?? 3001);
+  // const app = await NestFactory.create(RiderServiceModule);
+  const app = await NestFactory.createMicroservice(RiderServiceModule, {
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: 4002,
+    }
+  });
+
+  await app.listen();
 }
 bootstrap();
