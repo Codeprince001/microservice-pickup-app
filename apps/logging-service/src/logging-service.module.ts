7852285c2a85c53dd65eed4d0cd2ab4461075527
Wisdom <urangyorkwisdom@gmail.com>
2025-09-17 05:08:10 +0100

    chore(logging-service): import RiderCoordinatesModule and configure MongoDB connection

--- apps/logging-service/src/logging-service.module.ts
+++ apps/logging-service/src/logging-service.module.ts
@@ -2,11 +2,16 @@ import { Module } from '@nestjs/common';
 import { LoggingServiceController } from './logging-service.controller';
 import { LoggingServiceService } from './logging-service.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { RiderCoordinatesController } from '../rider-coordinates/rider-coordinates.controller';
+import { RiderCoordinatesModule } from '../rider-coordinates/rider-coordinates.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/27017/logs_db')],
-  controllers: [LoggingServiceController,RiderCoordinatesController],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://root:root@localhost:27017/logs_db?authSource=admin',
+    ),
+    RiderCoordinatesModule, 
+  ],
+  controllers: [LoggingServiceController],
   providers: [LoggingServiceService],
 })
 export class LoggingServiceModule {}
