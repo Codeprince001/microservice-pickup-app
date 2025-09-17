e6c4eaf112a2d99d2054685ffd7a967b669a9514
Wisdom <urangyorkwisdom@gmail.com>
2025-09-17 05:08:09 +0100

    chore(logging-service): configure RiderCoordinatesModule with Mongoose and RiderService client

--- apps/logging-service/rider-coordinates/rider-coordinates.module.ts
+++ apps/logging-service/rider-coordinates/rider-coordinates.module.ts
@@ -3,12 +3,23 @@ import { RiderCoordinatesController } from './rider-coordinates.controller';
 import { RiderCoordinatesService } from './rider-coordinates.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RiderCoordinates, RiderCoordinatesSchema } from './schemas/rider-coordinates.schema';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: RiderCoordinates.name, schema: RiderCoordinatesSchema },
     ]),
+      ClientsModule.register([
+        {
+          name: 'RIDER_SERVICE',
+          transport: Transport.TCP,
+          options: {
+            host: '0.0.0.0',
+            port: 4002,
+          },
+        },
+      ]),
   ],
   controllers: [RiderCoordinatesController],
   providers: [RiderCoordinatesService],
