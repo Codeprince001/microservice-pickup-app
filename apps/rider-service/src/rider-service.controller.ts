dd949b19a6652592c7ff6ee6d8011bb77b3f3857
Wisdom <urangyorkwisdom@gmail.com>
2025-09-17 05:08:11 +0100

    feat(rider-service): add MessagePattern handler for get_rider_by_id

--- apps/rider-service/src/rider-service.controller.ts
+++ apps/rider-service/src/rider-service.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { RiderServiceService } from './rider-service.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class RiderServiceController {
   constructor(private readonly riderServiceService: RiderServiceService) {}
 
-  @Get()
-  getHello(): string {
-    return this.riderServiceService.getHello();
-  }
+  // @Get(":id")
+  @MessagePattern({ cmd: 'get_rider_by_id' })
+  getRiderById(
+    data: { id: string },
+  ){
+    return this.riderServiceService.getRiderById(data.id);
+}
 }
